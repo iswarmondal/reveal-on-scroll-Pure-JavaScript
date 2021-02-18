7a59fcae7a25d83d6c3ce11a33c4d20e6d6663f6
Iswar <rick731202@gmail.com>
2021-02-18 17:56:32 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', () => {
+    let content = document.querySelector('.container');
+    let contentPos = content.getBoundingClientRect().top;
+    let screenPos = window.innerHeight / 1.2;
+    if(contentPos < screenPos) {
+        content.classList.add('active');
+    }
+    else{
+        content.classList.remove('active');
+    }
+})
