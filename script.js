074c68bec5f301cabf9599c1fa8edd678fd28797
Iswar <rick731202@gmail.com>
2021-03-30 19:40:29 +0530

    Formated the code nicely

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-window.addEventListener('scroll', () => {
-    let content = document.querySelector('.container');
-    let contentPos = content.getBoundingClientRect().top;
-    let screenPos = window.innerHeight / 1.2;
-    if(contentPos < screenPos) {
-        content.classList.add('active');
-    }
-    else{
-        content.classList.remove('active');
-    }
-})
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".container");
+  let contentPos = content.getBoundingClientRect().top;
+  let screenPos = window.innerHeight / 1.2;
+  if (contentPos < screenPos) {
+    content.classList.add("active");
+  } else {
+    content.classList.remove("active");
+  }
+});
